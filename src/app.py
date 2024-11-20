a4c95d9154f30670dc9e9e822673a6e2366801c5
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-11-20 17:04:29 +0200

    Add initial support for additional citation types

--- src/app.py
+++ src/app.py
@@ -1,23 +1,28 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
-from repositories.citation_repository import get_citations, create_citation
+from repositories.citation_repository import get_citations, create_citation, get_citation_types
 from config import app, test_env
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     citations = get_citations()
-    return render_template("index.html", citations=citations) 
+    all_citation_types = get_citation_types()
+    citation_type = None
 
-
+    if request.method == "POST":
+        citation_type = request.form["citation_type"]
+    return render_template("index.html", citations=citations, citation_type=citation_type, all_citation_types=all_citation_types)
 
 @app.route("/create_citation", methods=["POST"])
 def citation_creation():
+    citation_type = request.form.get("citation_type")
     author = request.form.get("author")
     title = request.form.get("title")
-    print(author, title)
+    year = request.form.get("year")
+    print(citation_type, author, title, year)
 
     try:
-        create_citation(author, title)
+        create_citation(citation_type, author, title, year)
         return redirect("/")
     except Exception as error:
         print(error)
--- src/entities/citation.py
+++ src/entities/citation.py
@@ -1,8 +1,10 @@
 class Citation:
-    def __init__(self, id, author, title):
+    def __init__(self, id, citation_type, author, title, year):
         self.id = id
+        self.citation_type = citation_type
         self.author = author
         self.title = title
+        self.year = year
 
     def __str__(self):
         return f"{self.title} by {self.author}"
