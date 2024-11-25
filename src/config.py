cc7c4da9287291ecfb32c2cc0a79e0b0147d85de
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-11-25 14:57:51 +0200

    Add folder and path for exported citations

--- src/config.py
+++ src/config.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from os import getenv
+import os
 
 
 load_dotenv()
 
-test_env = getenv("TEST_ENV") == "true"
+test_env = os.getenv("TEST_ENV") == "true"
 print(f"Test environment: {test_env}")
 
+dirname = os.path.dirname(__file__)
+EXPORT_FILE_NAME = "citations.bib"
+EXPORT_FILE_PATH = os.path.join(dirname, "..", "data", EXPORT_FILE_NAME)
+
 app = Flask(__name__)
-app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
