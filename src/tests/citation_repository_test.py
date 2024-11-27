9ee251471b5515f82874cc53341b34cb90c5df67
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-11-28 01:25:57 +0200

    Add tests for citekey and bibtex entry generation

---
+++ src/tests/citation_repository_test.py
@@ -0,0 +1,30 @@
+import unittest
+from repositories import citation_repository
+from entities.citation import Citation
+
+class TestCitationRepository(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_generate_citekey_uses_author_title_year_id(self):
+        citekey = citation_repository.generate_citekey("Author", "Title", "2024", 1)
+
+        self.assertEqual(citekey, "AuthorTitle1_2024")
+    
+    def test_generate_citekey_multiword_title_only_first_letters(self):
+        citekey = citation_repository.generate_citekey("Author", "Title of Source", "2024", 1)
+
+        self.assertEqual(citekey, "AuthorToS1_2024")
+
+    def test_generate_bibtex_entry_generates_valid_bibtex(self):
+        fields = {"author": "Author", "title": "Title", "year": "2024", "journal": "Journal"}
+        citation = Citation(1, "article", "AuthorTitle1_2024", fields)
+
+        tested_bibtex = citation_repository.generate_valid_bibtex_entry(citation)
+
+        good_bibtex = ("@article{AuthorTitle1_2024,\n"
+                    "    author = {Author},\n    title = {Title},\n"
+                    "    year = {2024},\n    journal = {Journal}\n}\n\n"
+        )
+
+        self.assertEqual(tested_bibtex, good_bibtex)
