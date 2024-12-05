c737cdc86950fe54e88d9f79667edc165ab28832
Axel <axel.lindroos1@gmail.com>
2024-12-05 14:21:24 +0200

    add link to ai helper

--- src/config.py
+++ src/config.py
@@ -8,9 +8,9 @@ import google.generativeai as genai
 load_dotenv()
 
 test_env = os.getenv("TEST_ENV") == "true"
-api_env = os.getenv("API_KEY") == "true"
+#api_env = os.getenv("API_KEY") == "true"
 print(f"Test environment: {test_env}")
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # dirname = os.path.dirname(__file__)
 # EXPORT_FILE_NAME = "citations.bib"
